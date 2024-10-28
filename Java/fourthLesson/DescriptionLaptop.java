10b52577e0e38ed3c842cddc496cf941a44996a4
Nurs0305n <146505309+Nurs0305n@users.noreply.github.com>
2024-10-28 18:15:56 +0600

    Update DescriptionLaptop.java

--- Java/fourthLesson/DescriptionLaptop.java
+++ Java/fourthLesson/DescriptionLaptop.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class DescriptionLaptop{
   public static void main (String[] args){
-    
+	  Scanner scna = new Scanner(System.in);
+
+	  int randomNumber = (int) (Math.random() * 5 + 1);
+
+      System.out.print("Отгадайте число: ");
+      int userNumber = scan.nextInt();
+
+      boolean checkAnswer = randemNumber == userNumber;
+
+      System.out.pritn(checkAnswer);
   }
 }
