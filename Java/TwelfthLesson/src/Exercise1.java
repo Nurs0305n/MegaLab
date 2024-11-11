ef71168e96c2244e334cfd033e086260be27802b
Nurs0305n <boruto200305+github@gmail.com>
2024-11-11 20:00:13 +0600

    11.11.24

---
+++ Java/TwelfthLesson/src/Exercise1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int userNumber, countIdenticalNumber = 1, identicalNumber = 0, maxCountIdenticalNumber = 0;
+
+        do {
+            userNumber = scanner.nextInt();
+            if (userNumber  == identicalNumber)
+                countIdenticalNumber++;
+            else {
+                identicalNumber = userNumber;
+                countIdenticalNumber = 1;
+            }
+
+            if (countIdenticalNumber > maxCountIdenticalNumber)
+                maxCountIdenticalNumber = countIdenticalNumber;
+        }while (userNumber != 0);
+
+        System.out.println(maxCountIdenticalNumber);
+    }
+}
