009c7e0cb68e891be2bc0e03b5af561b499bd58c
Nurs0305n <boruto200305+github@gmail.com>
2024-11-15 07:53:47 +0600

    Lesson 14.11.24

--- Java/thirteenthLesson/src/ArrayExerciseFour.java
+++ Java/thirteenthLesson/src/ArrayExerciseFour.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ArrayExerciseFour {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double d = scanner.nextDouble();
+        System.out.println(d);
     }
 }
