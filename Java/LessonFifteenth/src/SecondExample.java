c49472c7225c8458377c9f3892d0208210c64f6e
Nurs0305n <boruto200305+github@gmail.com>
2024-11-20 11:13:17 +0600

    complete homework 20.11.24

--- Java/LessonFifteenth/src/SecondExample.java
+++ Java/LessonFifteenth/src/SecondExample.java
@@ -1,27 +1,30 @@
 public class SecondExample {
     public static void main(String[] args) {
-        int x, y, moveX, moveY, nextX, nextY;
+        int x, y, move, lastMove = 0;
         int[][] field = new int[10][10];
-        x = (int)(Math.random() * 10 + 1);
-        y = (int)(Math.random() * 10 + 1);
+        x = (int) (Math.random() * 10);
+        y = (int) (Math.random() * 10);
         field[x][y] = 1;
 
-        nextX = x;
-        nextY = y;
         for (int i = 0; i < 3; i++) {
-            moveX = (int) (Math.random() * 3 - 2);
-            moveY = (int) (Math.random() * 3 - 2);
-            nextX += moveX;
-            nextY += moveY;
-            if ((moveX == 0 || moveY == 0) && ((nextX == x || nextY == y) && (nextY < 10 && nextX < 10 && nextX > -1 && nextY > -1))){
-                field[nextX][nextY] = 1;
-                x = nextX;
-                y = nextY;
-            }
-
+            move = (int) (Math.random() * 4 + 1);
+            if (move == 1 && x < 9 && move != lastMove) {
+                field[++x][y] = 1;
+                lastMove = Math.abs(move - 5);
+            } else if (move == 2 && y < 9 && move != lastMove) {
+                field[x][++y] = 1;
+                lastMove = Math.abs(move - 5);
+            } else if (move == 3 && y > 0 && move != lastMove) {
+                field[x][--y] = 1;
+                lastMove = Math.abs(move - 5);
+            } else if (move == 4 && x > 0 && move != lastMove) {
+                field[--x][y] = 1;
+                lastMove = Math.abs(move - 5);
+            } else
+                i--;
         }
 
-        for (int i = 0; i <field.length; i++){
+        for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
                 System.out.print(" nums[" + i + "][" + j + "] = " + field[i][j]);
             }
